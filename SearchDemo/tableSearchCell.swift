584155c700697ab5427010b5530166b4dc963951
netset <netset@netsets-Mac-Gobinder.local>
2019-08-21 14:43:09 +0530

    first commit

---
+++ SearchDemo/tableSearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  tableSearchCell.swift
+//  SearchDemo
+//
+//  Created by netset on 12/08/19.
+//  Copyright © 2019 NetSet. All rights reserved.
+//
+
+import UIKit
+
+class tableSearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
